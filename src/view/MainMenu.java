739c81d2955cd5575fa9afbd4d98e30417d1cc44
tandak <tanda.kabanda@kcl.ac.uk>
2015-03-11 17:09:53 +0000

    Update MainMenu.java
    intersection and full simulation need looking at

--- src/view/MainMenu.java
+++ src/view/MainMenu.java
@@ -1,12 +1,14 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 
 import control.WorldController;
 
@@ -14,29 +16,51 @@ public class MainMenu extends JPanel implements ActionListener {
 	JButton tButton, roundButton, interButton, fullSimulation;
 	WorldController wControl;
 	CarSimView mainFrame;
+	JPanel buttonJPanel = new JPanel();
+	JTable table = new JTable(2,2);
+	
+	
 
 	public MainMenu(WorldController wControl, CarSimView mainFrame) {
 		this.wControl = wControl;
 		this.mainFrame = mainFrame;
-		tButton = new JButton("T Junction");
-		roundButton = new JButton("Roundabout");
-		interButton = new JButton("Intersection");
-		fullSimulation = new JButton("Full Simulation");
+		tButton = new CustomJButton("T Junction");
+		roundButton = new CustomJButton("Roundabout");
+		interButton = new CustomJButton("Intersection");
+		fullSimulation = new CustomJButton("Full Simulation");
+		
+		this.setBorder(BorderFactory.createMatteBorder(2,2, 2, 2,Color.BLACK));
+		buttonJPanel.setBorder(BorderFactory.createMatteBorder(2,2, 2, 2,Color.BLACK));
+		buttonJPanel.setBackground(Color.WHITE);
 		tButton.addActionListener(this);
-		this.add(tButton);
-		this.add(roundButton);
-		this.add(interButton);
-		this.add(fullSimulation);
+		roundButton.addActionListener(this);
+		interButton.addActionListener(this);
+		fullSimulation.addActionListener(this);
+		this.add(buttonJPanel);	
+
+		buttonJPanel.add(tButton);
+		buttonJPanel.add(roundButton);
+		buttonJPanel.add(interButton);
+		buttonJPanel.add(fullSimulation);
+
 	
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getSource().equals(tButton)) {
+		if (e.getSource().equals(tButton)) { //T Junction choosen by user
 			
 			mainFrame.simulationView();
 			wControl.setTJunction();
+		}else if(e.getSource().equals(roundButton)){ //Roundabout example chosen by user
+			mainFrame.simulationView();
+			wControl.setRoundAbout();
+		}else if(e.getSource().equals(interButton)){ //Intersection chooen by user
+			mainFrame.simulationView();
+			//If there views set up for intersecton 
+		}else if(e.getSource().equals(fullSimulation)){ //Full simulation chosen by user. 
+			
 		}
 	}
 }
