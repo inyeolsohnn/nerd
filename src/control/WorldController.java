e8408385e9f0267d6637b445176657764b1d3cad
tandak <tanda.kabanda@kcl.ac.uk>
2015-03-12 15:23:06 +0000

    Updated WorldController.java

--- src/control/WorldController.java
+++ src/control/WorldController.java
@@ -1,33 +1,31 @@
 package control;
 
-import java.awt.Graphics;
 import java.awt.geom.Point2D;
-import java.awt.geom.Point2D.Double;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import javax.swing.JFrame;
 
-import view.CarSimView;
+import model.Car;
+import model.CarPark;
 import model.CarWorld;
 import model.Road;
-import model.Car;
-import model.UnknownConnectionError;
+import model.StraightRoad;
+import model.TrafficLight;
 
 public class WorldController {
 	private CarWorld cWorld; // core model
 	private JFrame carView; // JFrame
 
 	public WorldController() {
-	
+
 		this.cWorld = this.createWorld();
-		
+
 	}
 
 	public CarWorld getcWorld() {
 		return cWorld;
 	}
-	
+
 	public void setcWorld(CarWorld cWorld) {
 		this.cWorld = cWorld;
 	}
@@ -48,24 +46,25 @@ public class WorldController {
 	public void start() {
 		this.cWorld.setStatus("running");
 	}
-	public void exit(){
+
+	public void exit() {
 		this.cWorld.setStatus("exit");
 	}
 
 	// program loop
 	public void simulate() throws InterruptedException {
-		
+
 		while (true) {
+			
 			if (cWorld.getStatus().equals("running")) {
 				update();
-				render();
+			
+				
 				Thread.sleep(20); // the timing mechanism
 									// needs improvement
-			}
-			else if (cWorld.getStatus().equals("paused")) {
+			} else if (cWorld.getStatus().equals("paused")) {
 				Thread.sleep(20);
-			}
-			else if(cWorld.getStatus().equals("exit")){
+			} else if (cWorld.getStatus().equals("exit")) {
 				return;
 			}
 		}
@@ -80,51 +79,74 @@ public class WorldController {
 		// TODO Auto-generated method stub
 		return this.cWorld.getRoads();
 	}
+	public ArrayList<TrafficLight> getLights() {
+		// TODO Auto-generated method stub
+		return this.cWorld.getLights();
+	}
 
 	private void update() {
 		System.out.println("updating everything in the world");
-		ArrayList<Car> cars = getCars();
-		ArrayList<Road> roads = getRoads();
-		
-
-	}
-
-	private void render() {
+		for(int i=0;i<cWorld.getCars().size();i++){
+			cWorld.getCars().get(i).move();
+			
+		}
 		this.carView.repaint();
 
 	}
-	
-	
-	
 
-	public static ArrayList<Road> bfsRoads(Road road,
-			Road destinationRoad) {
+	public static ArrayList<Road> bfsRoads(Road road, Road destinationRoad) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	//example case setup
-	public void setTJunction(){
-		//construct the world with road network
+
+	// example case setup
+	public void setTJunction() {
+		// construct the world with road network
 		this.cWorld.setStatus("paused");
 		this.cWorld.flush();
-		//add new roads and such
-		
-		
-	}
+		// add new roads and such
+		Road sr = new StraightRoad(new Point2D.Float(100, 100),
+				new Point2D.Float(800, 100), 1, 1, this.getcWorld());
+		Road ar = new StraightRoad(new Point2D.Float(450, 130),
+				new Point2D.Float(450, 830), 1, 1, this.getcWorld());
+		CarPark cp = null;
 	
-	public void setRoundAbout(){
-		//first construct the world with road network
+		
+		
+		Car c1 = new Car(sr.getLanes().get(0).getStart(),10,sr.getLanes().get(0),
+				cp,this.cWorld,sr.getLanes().get(0).getStart());
+		c1.setCurrentSpeed(30);
+		this.cWorld.addCar(c1);
+		this.cWorld.addRoad(sr);
+		this.cWorld.addRoad(ar);
+
+	}
+	public void setIntersection(){
 		this.cWorld.setStatus("paused");
 		this.cWorld.flush();
-	
+		// add new roads and such
+		Road r1 = new StraightRoad(new Point2D.Float(50, 350),
+				new Point2D.Float(760, 350), 2, 1, this.getcWorld());
+		Road r2 = new StraightRoad(new Point2D.Float(380, 20),
+				new Point2D.Float(380, 700), 2, 1, this.getcWorld());
+		Road r3 = new StraightRoad(new Point2D.Float(790, 20),
+				new Point2D.Float(790, 700), 2, 1, this.getcWorld());
+		this.cWorld.addRoad(r1);
+		this.cWorld.addRoad(r2);
+		this.cWorld.addRoad(r3);
 	}
-	public CarWorld createWorld(){
-		if(this.cWorld==null){
-			CarWorld world= new CarWorld();
-			this.cWorld=world;
+
+	public void setRoundAbout() {
+		// first construct the world with road network
+
+	}
+
+	public CarWorld createWorld() {
+		if (this.cWorld == null) {
+			CarWorld world = new CarWorld();
+			this.cWorld = world;
 			return cWorld;
-		}else{
+		} else {
 			return this.cWorld;
 		}
 	}
